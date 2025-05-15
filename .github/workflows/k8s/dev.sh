e977ab9a55dc6c0ecd8ae88b3be092d8b4c42db4
meyuhas <itzhak@enclaive.io>
2025-05-15 13:12:55 +0200

    use ecr

--- .github/workflows/k8s/dev.sh
+++ .github/workflows/k8s/dev.sh
@@ -3,20 +3,38 @@
 # Load common functions and variables
 source "$(dirname "$0")/common.sh"
 
+# ECR configuration
+AWS_ACCOUNT_ID="886093416603"
+AWS_REGION="eu-central-1"
+ECR_REPO="emcp"
+IMAGE_TAG="dev"  # Use simple tag
+
 echo "=== Deploying to DEVELOPMENT Environment ==="
 NAMESPACE="emcp-dev"
 
 # Ensure namespace exists
 ensure_namespace "$NAMESPACE"
 
-# Deploy Admin service
-echo "Deploying Admin service..."
+# Create AWS ECR pull secret
+echo "Creating ECR pull credentials..."
+TOKEN=$(aws ecr get-login-password --region ${AWS_REGION})
+kubectl create secret docker-registry aws-ecr-creds \
+  --docker-server=${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com \
+  --docker-username=AWS \
+  --docker-password="${TOKEN}" \
+  --namespace=${NAMESPACE} \
+  --dry-run=client -o yaml | kubectl apply -f -
+
+# Deploy Admin service from ECR
+echo "Deploying Admin service from ECR..."
 helm upgrade --install admin ./charts/admin \
   --namespace "$NAMESPACE" \
   --create-namespace \
   --values ./charts/admin/environments/values.dev.yaml \
-  --set image.tag="dev" \
-  --set imagePullSecrets[0].name=harbor-creds \
+  --set image.repository="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_REPO}" \
+  --set image.tag="${IMAGE_TAG}" \
+  --set image.pullPolicy=Always \
+  --set imagePullSecrets[0].name=aws-ecr-creds \
   --set environment=dev \
   --set logging.level=debug \
   --set logging.format=pretty \
